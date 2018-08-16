accd0450234ae2f032e460beb4c2277d7839e570
mb <smbradtmichael@gmail.com>
2018-08-16 16:44:14 -0700

    convert forEach method to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,3 @@
-// Specs
-// The program recognizes vowels in a multiple-word sentence, regardless of capitalization.
-// Input Example: "CATS CATERED THE EVENT"
-// Output Example: 7
-
-// The program recognizes a single vowel in a multiple-character word.
-// Input Example: "cat"
-// Output Example: 1
-// The program recognizes a single vowel.
-// Input Example: "a"
-// Output Example: 1
-// The program recognizes multiple vowels in a single word.
-// Input Example: "cater"
-// Output Example: 2
-// The program recognizes a single vowel, regardless of case.
-// Input Example: "A"
-// Output Example: 1
-// The program recognizes all vowels in a multiple-word sentence, regardless of inconsistent capitalization.
-// Input Example: "CaTS CATEReD ThE EveNT"
-// Output Example: 7
-// The program ignores non-alphabetical characters, since they cannot be vowels.
-// Input Example: "4%"
-// Output Example: 0
-// The program recognizes vowels in a multiple-word sentence.
-// Input Example: "cats catered the event"
-// Output Example: 7
-
-
 
 $(document).ready(function() {
   $("form#words").submit(function(event){
@@ -34,17 +6,23 @@ $(document).ready(function() {
     var userWords = [];
 
     $("input#userInput").each(function(){
-      var vowels = $(this).val();
-      userWords.push(vowels);
+      var userInput = $(this).val();
+      userWords.push(userInput);
     });
 
-    results = userWords.toString().split("");
-    results.forEach(function(letter){
+    results = userWords.toString().toLowerCase().split("");
+    // results.forEach(function(letter){
+    //   if (letter === "a" || letter ==="e" || letter ==="i" || letter ==="o" || letter ==="u") {
+    //     vowels ++;
+    //   }
+    //
+    // });
+    for (i = 0; i < results.length; i ++) {
+      var letter = results[i]; //checks the var in the index possition
       if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
-        vowels ++;
+        vowels += 1;
       }
-    });
-
+    }
     console.log(results);
     console.log(vowels);
   });
